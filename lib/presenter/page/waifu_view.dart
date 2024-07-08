f35acda192cb91b08a9506868d0f82f72b97a644
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2024-07-08 18:10:41 -0300

    [RESPONSIVIDA] - Melhorando a responsividade.

--- lib/presenter/page/waifu_view.dart
+++ lib/presenter/page/waifu_view.dart
@@ -12,15 +12,6 @@ class WaifuView extends StatefulWidget {
 class _WaifuViewState extends State<WaifuView> {
   final store = WaifuStore();
 
-  
-
-  //PEGAR AS INFORMAÇÕES DA CONTROLLER -> STORE, FAZER MOSTRAR NA TELA ESSAS INFORMAÇÕES.
-  // STORE -> GETWAIFU -> TODA VEZ QUE EU APERTAR UM BOTÃO APARECER UM LOADING -> MUDAR DE WAIFU.
-  // CLOSURE -> FUNÇÃO ANONIMA QUE RETORNA OUTRA FUNÇÃO. -> CALLBACK
-  // function teste(callback){
-  //      callback();
-  // } 
-
   @override
   void initState()  {
     store.addListener((){
@@ -40,9 +31,9 @@ class _WaifuViewState extends State<WaifuView> {
      case WaifuState.INIT:
        return const Center(child: Text('Press the button to get a waifu'));
      case WaifuState.LOADING:
-       return const Center(child: CircularProgressIndicator());
+       return Container(width: MediaQuery.of(context).size.width - 20,height: MediaQuery.of(context).size.height/2, child: const Center(child: CircularProgressIndicator()));
      case WaifuState.SUCCESS:
-       return Image.network(store.waifu.url!,errorBuilder: (context, error, stackTrace) {
+       return Image.network(store.waifu.url!,width: MediaQuery.of(context).size.width - 20,height: MediaQuery.of(context).size.height/2,errorBuilder: (context, error, stackTrace) {
           return const Text('Failed to load image');
        });
      case WaifuState.FAILURE:
